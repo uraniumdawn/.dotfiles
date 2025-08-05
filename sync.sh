ede215064864136bd03814cab2755bb3e223e496
monolith <sirozhaua@gmail.com>
2025-08-05 15:25:28 +0300

    Add vscode files

--- sync.sh
+++ sync.sh
@@ -8,3 +8,5 @@ cp ~/.oh-my-zsh/custom/themes/uranium.zsh-theme ./.uranium.zsh-theme
 cp ~/.config/alacritty/alacritty.toml ./.alacritty.toml
 cp ~/.config/k9s/config.yaml ./.k9s/config.yaml
 cp ~/.config/k9s/skins/uraniumv2.yaml ./.k9s/skins/uraniumv2.yaml
+cp ~/Library/Application\ Support/VSCodium/User/settings.json ./.vscode/settings.json
+cp ~/Library/Application\ Support/VSCodium/User/keybindings.json ./.vscode/keybindings.json
